66cf7983594ae3defdd7335f0f5992d4d834c6e1
Noman Sadiq <nomi.cs786@gmail.com>
2025-07-07 23:37:41 +0400

    query syntax error fix

--- queries/pre.sql
+++ queries/pre.sql
@@ -1,4 +1,4 @@
 ALTER TABLE expense_report ADD COLUMN expensed_date TEXT;
 
 UPDATE expense_report
-SET expensed_date = datetime(expensed / 1000, 'unixepoch
+SET expensed_date = datetime(expensed / 1000, 'unixepoch')
